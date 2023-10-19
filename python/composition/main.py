e21eb88fc1d4540194fce021fb9e991679e22037
Thomas Loo <tom@TomsPC>
2023-10-19 21:49:09 +1100

    Add BeyBlade class with constructor with randomised stats and modifiers

--- python/composition/main.py
+++ python/composition/main.py
@@ -6,6 +6,7 @@ frodo = rpg.Burglar("Frodo", "Hobbit", 50, 25)
 saruman = rpg.Wizard("Saruman", "Human", 80, 100)
 
 frodo.inv.set_currency(9, 47, 23)
+aragorn.inv.set_currency(20, 50, 0)
 
 chest = rpg.Chest(["Long sword", "Iron helm"], 2, 25, 50)
 
@@ -17,13 +18,17 @@ galadriel.battle(frodo)
 print(galadriel.mana)
 
 # print(aragorn.__dict__)
-# print(galadriel.__dict__)
+print(galadriel.__dict__)
 # print(frodo.__dict__)
 
 # print(chest.inv.__dict__)
 
 # Frodo loots a chest!
-# print(chest.inv.transfer(frodo.inv))
+print(chest.inv.transfer(frodo.inv))
+
+
+
+
 
 # print(frodo.inv.__dict__)
 # print(frodo.inv.get_currency())
