fb73646eff8da8891445725584adef81027b6c26
Thomas Loo <tom@TomsPC>
2023-11-21 14:17:59 +1100

    Add Users and register route

--- flask-lessons/trello-clone/app.py
+++ flask-lessons/trello-clone/app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
 from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
@@ -9,6 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:spameg
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+bcrypt = Bcrypt(app)
 
 class Card(db.Model):
     __tablename__ = 'cards'
@@ -23,6 +25,19 @@ class CardSchema(ma.Schema):
     class Meta:
         fields = ('id', 'title', 'description', 'status', 'date_created')
 
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'email', 'password', 'is_admin')
+
 @app.cli.command('db_create')
 def db_create():
     db.drop_all()  # Ensures database is CLEAN before creating a new database
@@ -30,7 +45,22 @@ def db_create():
     print('Created tables')  # Add GRANT ALL ON SCHEMA public TO trello_dev in trello table;
 
 @app.cli.command('db_seed')  # Seeding is adding test data
+
 def db_seed():
+    users = [
+        User (
+            email = 'admin@spam.com',
+            password = bcrypt.generate_password_hash('spinynorman').decode('utf8'),
+            is_admin = True
+        ),
+
+        User (
+            name = 'John Cleese',
+            email = 'cleese@spam.com',
+            password = bcrypt.generate_password_hash('tisbutascratch').decode('utf8'),
+        )
+    ]
+
     cards = [
         Card(
             title = 'Start the project',
@@ -54,9 +84,32 @@ def db_seed():
         )
     ]
     db.session.add_all(cards)
+    db.session.add_all(users)
     db.session.commit()
 
     print('Datebase seeded')
+
+@app.route('/users/register', methods=['POST'])
+def register():
+    # Parse incoming POST body through the schema
+    user_info = UserSchema().load(request.json)
+    # Create a new user with the parsed data
+    user = User(
+        email = user_info['email'],
+        password = bcrypt.generate_password_hash(user_info['password']).decode('utf8'),
+        name = user_info.get('name', '')
+    )
+
+    # print(user)
+    # print(user.__dict__)
+    # return 'ok', 201
+
+    # Add and commit the new user to the database (remember sessions?)
+    db.session.add(user)
+    db.session.commit()
+
+    # Return the new user
+    return UserSchema(exclude=['password']).dump(user), 201
 
 @app.route('/cards')
 def all_cards():
@@ -68,3 +121,4 @@ def all_cards():
 @app.route('/')
 def index():
     return 'Hello world'
+
