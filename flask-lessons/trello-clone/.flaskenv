447e5e42448fc4589057b4a0f9a27191951d494e
Thomas Loo <tom@TomsPC>
2023-11-23 12:17:17 +1100

    Authnetication lesson code along

--- flask-lessons/trello-clone/.flaskenv
+++ flask-lessons/trello-clone/.flaskenv
@@ -1,2 +1,4 @@
-FLASK_RUN_PORT=5555
-FLASK_DEBUG=true
+FLASK_RUN_PORT=5555  # Flask server port
+FLASK_DEBUG=true  # Set TRUE for debug mode
+
+JWT_KEY="Ministry of Silly Walks"  # JWT signing key
--- flask-lessons/trello-clone/app.py
+++ flask-lessons/trello-clone/app.py
@@ -1,16 +1,36 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
 from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
+from sqlalchemy.exc import IntegrityError
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from datetime import timedelta
+from os import environ
 
 app = Flask(__name__)
+
+app.config['JWT_SECRET_KEY'] = environ.get('JWT_KEY')
+
+# app.config['JWT_SECRET_KEY'] = 'Ministry of Silly Walks'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:spameggs123@127.0.0.1:5432/trello'
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+
+def admin_required():
+    user_email = get_jwt_identity()  # Will get the email address of the user that has sent that token
+    stmt = db.select(User).where(User.email == user_email)  # Email taken from the token; because the identity is the user.email as specified in the create_access_token code
+    user = db.session.scalar(stmt)  # Will get an instance of the user model
+    if not user.is_admin:  # Compares the is_admin flag from the stmt which has taken the instance of the user model
+        return abort(401)
+
+@app.errorhandler(401)
+def unauthorized(err):
+    return {'error' : 'You are not authorised to access this resource'}
 
 class Card(db.Model):
     __tablename__ = 'cards'
@@ -91,28 +111,52 @@ def db_seed():
 
 @app.route('/users/register', methods=['POST'])
 def register():
-    # Parse incoming POST body through the schema
-    user_info = UserSchema().load(request.json)
-    # Create a new user with the parsed data
-    user = User(
-        email = user_info['email'],
-        password = bcrypt.generate_password_hash(user_info['password']).decode('utf8'),
-        name = user_info.get('name', '')
-    )
+    try:
+        # Parse incoming POST body through the schema
+        user_info = UserSchema(exclude =['id', 'is_admin']).load(request.json)
+        # Create a new user with the parsed data
+        user = User(
+            email = user_info['email'],
+            password = bcrypt.generate_password_hash(user_info['password']).decode('utf8'),
+            name = user_info.get('name', '')
+        )
 
-    # print(user)
-    # print(user.__dict__)
-    # return 'ok', 201
+        # print(user)
+        # print(user.__dict__)
+        # return 'ok', 201
 
-    # Add and commit the new user to the database (remember sessions?)
-    db.session.add(user)
-    db.session.commit()
+        # Add and commit the new user to the database (remember sessions?)
+        db.session.add(user)
+        db.session.commit()
 
-    # Return the new user
-    return UserSchema(exclude=['password']).dump(user), 201
+        # Return the new user
+        return UserSchema(exclude=['password']).dump(user), 201
+    except IntegrityError:
+        return {'error' : 'Email address already in use'}, 409
+
+@app.route('/users/login', methods=['POST'])
+def login():
+    # 1. Parse incoming POST body through the schema
+    user_info = UserSchema(exclude=['id', 'name', 'is_admin']).load(request.json)
+    # 2. Select user with email that matches the one in the POST body
+    stmt = db.select(User).where(User.email == user_info['email'])
+    user = db.session.scalar(stmt)
+    # 3. Check password hash
+    if user and bcrypt.check_password_hash(user.password, user_info['password']):
+        # 4. Create JSON Web Token (JWT)
+        token = create_access_token(identity = user.email, expires_delta = timedelta(hours = 2))
+        # 5. Return the token
+        return {'token' : token, 'user' : UserSchema(exclude = ['password']).dump(user)}
+    else:
+        return {'error' : 'Invalid email or password'}, 401
+    
+    
+
 
 @app.route('/cards')
+@jwt_required()
 def all_cards():
+    admin_required()
     # select * from cards;
     stmt = db.select(Card).where(db.or_(Card.status != 'Done', Card.id > 2)).order_by(Card.title.desc())
     cards = db.session.scalars(stmt).all()
