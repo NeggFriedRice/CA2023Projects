cd71a2a11e8abb2496e919911e809c3927d1cc40
Thomas Loo <tom@TomsPC>
2023-12-20 11:12:44 +1100

    Modularise functions into Utils class variable(?)

---
+++ javascript/intro/script.js
@@ -0,0 +1,36 @@
+let str = "Hello World!"
+
+// console.log(str.replaceAll('o', '---'))
+
+
+// console.log(`Hello, ${str}!`)
+// console.log(`Answer: ${5 * 10}`)
+
+// console.log(Math.floor(3/2))
+
+// let x = 5
+// console.log(x++)
+
+// console.log(x)
+
+const Utils = {
+    add: (x, y) => x + y,
+    double: (x) => x * 2,
+    squares: (arr) => arr.map(x => x ** 2)
+}
+const add = (x, y) => x + y
+// const square = (x) => x ** 2
+const double = (x) => x * 2
+// const result = numbers.map(double) becomes the below function
+const squares = arr => arr.map(x => x ** 2)
+
+// function add(x, y) {
+// return x + y
+// }
+
+console.log(add(10, 34))
+// console.log(square(10))
+
+const numbers = [12, 50, 44, 32, 2]
+const result = Utils.squares(numbers)
+console.log(result)
