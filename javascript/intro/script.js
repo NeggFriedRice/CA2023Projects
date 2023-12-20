f74d7728d3fd0e28678f36246545c02810109301
Thomas Loo <tom@TomsPC>
2023-12-20 11:17:27 +1100

    const [first, second] = people

--- javascript/intro/script.js
+++ javascript/intro/script.js
@@ -37,7 +37,10 @@
 
 const people = ["Matt", "John", "Mary", "Kate"]
 
-first = people[0]
-second = people[1]
+// first = people[0]
+// second = people[1]
+
+// Create a variable called first and second, and take the first element and second element from people
+const [first, second] = people
 
 console.log(first, second)
