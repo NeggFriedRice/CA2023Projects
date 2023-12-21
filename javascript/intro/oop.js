ad6048fe26dee473f402bff97c3848f1db0341ed
Thomas Loo <tom@TomsPC>
2023-12-21 11:05:39 +1100

    Using JS class + constructor

--- javascript/intro/oop.js
+++ javascript/intro/oop.js
@@ -4,11 +4,26 @@
 
 // x = Foo()
 
-function Person(name, age) {
-    this.name = name
-    this.age = age
+// function Person(name, age) {
+//     this.name = name
+//     this.age = age
+
+//     this.greet = () => {
+//         console.log(`${this.name} is ${this.age} years old`)
+//     }
+// }
+
+// const person = new Person("Matt", 51)
+// person.age = 52
+
+// person.greet()
+
+class Person {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
 }
 
 const person = new Person("Matt", 51)
-
 console.log(person)
