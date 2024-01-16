f28c2a0ee459dba9450be203758160f077e81843
Thomas Loo <tom@TomsPC>
2024-01-16 14:23:09 +1100

    Callback function lesson notes

--- javascript/callbacks/script.js
+++ javascript/callbacks/script.js
@@ -10,9 +10,25 @@
 
 // console.log('Done')
 
-const req = new XMLHttpRequest()
-req.open("GET", 'https://icanhazdadjoke.com/')
-req.setRequestHeader('Accept', 'application/json')
-req.send()
+function getJoke(cb) {
+    // 2. Create a new XHR object
+    const req = new XMLHttpRequest()
+    // 3. Add listener for when the response is received and parsed
+    // 9. Listener callback is triggered (event => cb(event.target.response.joke)), which in turn calls cb, passing the joke
+    req.addEventListener("load", event => cb(event.target.response.joke))
+    // 4. Open the URL with the appropriate verb
+    req.open("GET", 'https://icanhazdadjoke.com/')
+    // 5. Set Accept header so server gives us JSON
+    req.setRequestHeader('Accept', 'application/json')
+    // 6. Set responseType so XHR object parses the JSON
+    req.responseType = 'json'
+    // 7. Send the request, then immediately return from getJoke (i.e. don't wait!)
+    req.send()
+}
 
+// 1. Call getJoke and pass a callback function
+// 10. Callback is called via cb in the load listener in getJoke, receiving the joke
+getJoke(joke => console.log(joke))
+
+// 8. getJoke returned immediately
 console.log('Request sent!')
