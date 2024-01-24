3e8a03a39ef5da7ca8248ee4cd60623ded7313c7
Thomas Loo <tom@TomsPC>
2024-01-25 01:39:15 +1100

    Pokemon API validator challenge completed

--- javascript/edContentAndChallenges/custom-validator/index.js
+++ javascript/edContentAndChallenges/custom-validator/index.js
@@ -7,15 +7,37 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
+// Custom validator
+app.use((request, response, next) => {
+    let importantInfo = {
+        url: request.originalUrl,
+        verb: request.method,
+        ip: request.ip
+    }
+    console.log(importantInfo)
+    next()
+})
+
+
 
 const baseURL = "https://pokeapi.co/api/v2/pokemon/";
 
 // localhost:3000
-app.post('/', async (request, response) => {
+app.post('/', 
+    body('pokemonName').trim().escape().not().isEmpty().isLength({min: 2}).withMessage("Pokemon name must be at least 2 characters long and not empty!"),
+    async (request, response) => {
+    const errors = validationResult(request)
+    const errorsArray = errors.array()
+    for (let i = 0; i < errorsArray.length; i++) {
+        console.log(errorsArray[i].msg)
+    }
 
-    // If this fetch request returns JSON, then we want to store it as an object that we can work with.
-    // let result = await fetch(baseURL + request.body.pokemonName).then((data) => {return data.json()});
-    let result = await fetch(baseURL + request.body.pokemonName)
+    if (!errors.isEmpty()) {
+        return response.status(400).json({errors: errors.array()})
+    } else {
+        // If this fetch request returns JSON, then we want to store it as an object that we can work with.
+        // let result = await fetch(baseURL + request.body.pokemonName).then((data) => {return data.json()});
+        let result = await fetch(baseURL + request.body.pokemonName)
         .then((data) => {
             if (data.statusText == "Not Found") {
                 throw new Error("Pokemon not found")
@@ -30,7 +52,11 @@ app.post('/', async (request, response) => {
             })}
         ))
         .catch((error) => {response.json({error: error.message})})
-    })     
+    }
+
+
+
+})     
 
 
 
