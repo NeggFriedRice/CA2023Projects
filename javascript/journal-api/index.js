352347926e11064345cd53b6e859dae78e2189ba
Thomas Loo <tom@TomsPC>
2024-01-25 13:55:17 +1100

    TODO notes

--- javascript/journal-api/index.js
+++ javascript/journal-api/index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => res.send({ info: "Journal API" }))
 
 // TOD: Move /categories into routes folder
 // TODO: Complete categories CRUD
-// TODO: ADVANCE: Add a route /categories/:id/entries that returns all entries in the given category
+// TODO: ADVANCE: Modify "GET /categories/:id" to embed an array of all the entries in that category
 
 app.get('/categories', async (req, res) => res.status(200).send(await CategoryModel.find()))
 
