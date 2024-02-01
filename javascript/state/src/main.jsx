d0e724d10067b164e9b3d97f0fd12d3a97fc6684
Thomas Loo <tom@TomsPC>
2024-02-01 23:20:18 +1100

    Pokemon search app

--- javascript/state/src/main.jsx
+++ javascript/state/src/main.jsx
@@ -2,9 +2,11 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 import "./index.css"
-import Demo from "./pages/index"
+// import Demo from "./pages/index"
+import PokeAPI from "./pages/pokeAPI.jsx"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <App />
-  <Demo />
+  // <Demo />
+  <PokeAPI />
 )
