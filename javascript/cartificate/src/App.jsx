2071aa4ec891283dd74887239920a1437908ca50
Thomas Loo <tom@TomsPC>
2024-02-07 21:33:50 +1100

    Create form, add in change handler

---
+++ javascript/cartificate/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+import InputForm from './components/InputForm'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <InputForm />
+    </>
+  )
+}
+
+export default App
