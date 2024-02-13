0d8f7ff9774d9a29a7d2607c5e69ad018e31a3bc
Thomas Loo <tom@TomsPC>
2024-02-13 20:50:29 +1100

    Add post and delete routes

--- javascript/cartificate-api/index.js
+++ javascript/cartificate-api/index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors())
 
-app.use('/update', router)
+app.use('/updates', router)
 
 app.get('/', (req, res) => {
   res.send({
--- javascript/cartificate-api/routes/update_routes.js
+++ javascript/cartificate-api/routes/update_routes.js
@@ -7,4 +7,13 @@ router.get('/', async (req, res) => {
   res.send(await UpdateModel.find())
 })
 
+router.post('/new', async (req, res) => {
+  try {
+    const newUpdate = await (await UpdateModel.create(req.body))
+    res.status(201).send(newUpdate)
+  } catch (error) {
+    console.log(e)
+  }
+})
+
 export default router
